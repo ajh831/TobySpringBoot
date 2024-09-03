7b9f9fdecd1537887c593b8a20e888acc89e4f2c
JUNGHOON AN <gnstm831@naver.com>
2024-09-03 15:15:12 +0900

    섹션5-1. 스프링 컨테이너 사용

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -7,20 +7,26 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		// 스프링 컨테이너 생성
+		GenericXmlApplicationContext applicationContext = new GenericXmlApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh(); // 컨테이너 초기화(빈 오브젝트 생성)
+
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-        	HelloController helloController = new HelloController();
-
             servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,14 +34,11 @@ public class HellobootApplication {
 					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
+						HelloController helloController = applicationContext.getBean(HelloController.class);
 						String ret = helloController.hello(name); // 파라미터 추출
 
-						resp.setStatus(HttpStatus.OK.value()); // 생략가능
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
 						resp.getWriter().println(ret); // 결과값을 웹 응답에 담음
-					}
-					else if (req.getRequestURI().equals("/user")) {
-						//
 					}
 					else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
