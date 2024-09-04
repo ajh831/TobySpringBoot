220b8817247bd3a07f6e17eddeb1bc2604c5d8be
JUNGHOON AN <gnstm831@naver.com>
2024-09-04 14:39:06 +0900

    섹션5-8. 자바코드 구성 정보 사용

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.http.HttpHeaders;
@@ -15,16 +18,26 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
+@Configuration
 public class HellobootApplication {
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너 생성
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh(); // 생략 X
@@ -38,8 +51,7 @@ public class HellobootApplication {
 				webServer.start(); // Tomcat Servelet Container 동작
 			}
 		};
-		applicationContext.registerBean(HelloController.class); // 빈 등록
-		applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
+		applicationContext.register(HellobootApplication.class);
 		applicationContext.refresh(); // 컨테이너 초기화(빈 오브젝트 생성)
 	}
 }
