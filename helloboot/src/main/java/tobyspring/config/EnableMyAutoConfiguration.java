9daf0036e1471551df294912926ff1df8b9b69ac
JUNGHOON AN <gnstm831@naver.com>
2024-09-06 16:30:19 +0900

    섹션7-5. 동적인 자동 구성 정보 등록

--- helloboot/src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
+++ helloboot/src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -13,6 +13,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+@Import(MyAutoConfigImportSeletor.class)
 public @interface EnableMyAutoConfiguration {
 }
+
