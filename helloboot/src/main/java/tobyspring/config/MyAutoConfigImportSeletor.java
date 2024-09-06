e5118f25fea53596937dbf1372994f0aec165b3a
JUNGHOON AN <gnstm831@naver.com>
2024-09-06 20:09:27 +0900

    섹션7-6. 자동 구성정보 파일 분리

--- helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSeletor.java
+++ helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSeletor.java
@@ -1,15 +1,35 @@
 package tobyspring.config;
 
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigImportSeletor implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSeletor(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "tobyspring.config.autoconfig.DispatcherServletConfig",
-                "tobyspring.config.autoconfig.TomcatWebServerConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+//        for(String candidates : ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+//        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(candidate ->
+//                autoConfigs.add(candidate)
+//                );
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
+//        return autoConfigs.stream().toArray(String[]::new);
+//        return Arrays.copyOf(autoConfigs.toArray(), autoConfigs.size(), String[].class);
     }
 }
