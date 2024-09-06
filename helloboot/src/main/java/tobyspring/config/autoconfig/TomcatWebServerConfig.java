e7fea29350dc0c855f77a336955e347234875513
JUNGHOON AN <gnstm831@naver.com>
2024-09-06 20:31:37 +0900

    섹션7-7. 자동 구성 애너테이션 적용

--- helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,11 +4,18 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tobyspring.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletContainer() {
         return new TomcatServletWebServerFactory();
     }
 }
+
+
+
+
+
+
